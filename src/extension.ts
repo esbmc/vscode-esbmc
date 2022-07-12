d61a615aa4a1d0face38de1f7ddd4655ba7af293
Joss Moffatt <jossmoff@hotmail.co.uk>
2022-07-12 16:21:55 +0100

    Fixed linting issues

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,16 @@
 /* eslint-disable require-jsdoc */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { registerCommands } from './commands/registerCommands';
+import * as vscode from 'vscode'
+import { registerCommands } from './commands/registerCommands'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "vscode-esbmc" is now active!');
+export function activate (context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "vscode-esbmc" is now active!')
   // Register Commands
-  context.subscriptions.push(...registerCommands());
+  context.subscriptions.push(...registerCommands())
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate () {}
