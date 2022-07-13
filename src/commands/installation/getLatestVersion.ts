135bf31537ff425bf58d2571b868f4e708ef81d5
Joss Moffatt <jossmoff@hotmail.co.uk>
2022-07-13 10:09:01 +0100

    Fixed linting issues

--- src/commands/installation/getLatestVersion.ts
+++ src/commands/installation/getLatestVersion.ts
@@ -1,8 +1,8 @@
-import { Request, default as fetch } from 'node-fetch';
+import { Request, default as fetch } from 'node-fetch'
 
-export async function getLatestVersion() {
-    let request = new Request("https://github.com/esbmc/esbmc/releases/latest");
-    const response = await fetch(request);
-    const redirUrl = response.url;
-    return redirUrl.split('/').pop();
+export async function getLatestVersion () {
+  const request = new Request('https://github.com/esbmc/esbmc/releases/latest')
+  const response = await fetch(request)
+  const redirUrl = response.url
+  return redirUrl.split('/').pop()
 }
--- src/commands/installation/install.ts
+++ src/commands/installation/install.ts
@@ -1,37 +1,36 @@
-import * as vscode from 'vscode';
-import { getApi, FileDownloader } from "@microsoft/vscode-file-downloader-api";
-import { getLatestVersion } from './getLatestVersion';
+import * as vscode from 'vscode'
+import { getApi, FileDownloader } from '@microsoft/vscode-file-downloader-api'
+import { getLatestVersion } from './getLatestVersion'
 
+export async function install (context: vscode.ExtensionContext) {
+  const fileDownloader: FileDownloader = await getApi()
+  const downloadingStatusIcon = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0)
+  downloadingStatusIcon.text = '$(loading~spin) Installing ESBMC '
+  downloadingStatusIcon.show()
+  const file: vscode.Uri = await fileDownloader.downloadFile(
+    vscode.Uri.parse('https://github.com/esbmc/esbmc/releases/latest/download/ESBMC-Linux.sh'),
+    'ESBMC-Linux.sh',
+    context
+  )
+  if (file === undefined) {
+    vscode.window.showErrorMessage('Could not download ESBMC')
+  }
 
-export async function install(context: vscode.ExtensionContext) {
-    const fileDownloader: FileDownloader = await getApi();
-    var downloadingStatusIcon = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-    downloadingStatusIcon.text = `$(loading~spin) Installing ESBMC `;
-    downloadingStatusIcon.show();
-    const file: vscode.Uri = await fileDownloader.downloadFile(
-        vscode.Uri.parse('https://github.com/esbmc/esbmc/releases/latest/download/ESBMC-Linux.sh'),
-        'ESBMC-Linux.sh',
-        context
-    );
-    if(file === undefined) {
-        vscode.window.showErrorMessage("Could not download ESBMC");
-    }
-    
-    const filePath = file.fsPath;
-    const makeExecCommand = `chmod +x ${filePath}`;
-    const setupInstallLocationCommand = `mkdir $HOME/bin/esbmc-folder`;
-    const installCommand = `${filePath} --skip-license --prefix=$HOME/bin/esbmc-folder`;
-    // TODO Ask if possibility to add a --just-binary flag to speed this process and make it safer
-    const moveCommand = `mv $HOME/bin/esbmc-folder/bin/esbmc $HOME/bin`;
-    const cleanupCommand = `rm -rf $HOME/bin/esbmc-folder`;
-    const terminal = vscode.window.createTerminal("ESBMC");
-    terminal.sendText(makeExecCommand);
-    terminal.sendText(setupInstallLocationCommand);
-    terminal.sendText(installCommand);
-    terminal.sendText(moveCommand);
-    terminal.sendText(cleanupCommand);
-    terminal.dispose()
-    downloadingStatusIcon.hide();
-    const version = getLatestVersion();
-    vscode.window.showInformationMessage(`Installed ESBMC ${version}`);
+  const filePath = file.fsPath
+  const makeExecCommand = `chmod +x ${filePath}`
+  const setupInstallLocationCommand = 'mkdir $HOME/bin/esbmc-folder'
+  const installCommand = `${filePath} --skip-license --prefix=$HOME/bin/esbmc-folder`
+  // TODO Ask if possibility to add a --just-binary flag to speed this process and make it safer
+  const moveCommand = 'mv $HOME/bin/esbmc-folder/bin/esbmc $HOME/bin'
+  const cleanupCommand = 'rm -rf $HOME/bin/esbmc-folder'
+  const terminal = vscode.window.createTerminal('ESBMC')
+  terminal.sendText(makeExecCommand)
+  terminal.sendText(setupInstallLocationCommand)
+  terminal.sendText(installCommand)
+  terminal.sendText(moveCommand)
+  terminal.sendText(cleanupCommand)
+  terminal.dispose()
+  downloadingStatusIcon.hide()
+  const version = getLatestVersion()
+  vscode.window.showInformationMessage(`Installed ESBMC ${version}`)
 }
